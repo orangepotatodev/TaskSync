3c8a2a2f99d4b3d12883db4958142713753fb365
orangepotatodev <orangepotato.dev@proton.me>
2025-04-11 17:48:14 +0500

    Delete app directory

--- app/components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-blue-500"></div>
-    </div>
-  );
-} 
--- app/providers/AuthProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import { AuthProvider as AuthContextProvider } from "../context/AuthContext";
-
-export function AuthProvider({ children }: { children: React.ReactNode }) {
-  return <AuthContextProvider>{children}</AuthContextProvider>;
-} 
